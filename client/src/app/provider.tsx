d3040b20663e27cc490116802903981bd7da99a9
unknown <rkawk1539@gmail.com>
2025-02-25 13:30:26 +0900

    프론트엔드 폴더구조 초기화 (Create-Next-App)

---
+++ client/src/app/provider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Provider = ({ children }: Props) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+            staleTime: 30 * 1000,
+            gcTime: 10 * 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default Provider;
